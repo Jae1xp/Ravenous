525f17245fcbe9c05b156e0aec13b44894caaf2f
Jae1xp <jae1xp@gmail.com>
2017-11-13 16:37:45 -0500

    added images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'; //imports React and it's component
+import React from 'react'; //imports React and it's component
 import './App.css'; //imports css styling
 //import logo from './logo.svg'; //imports the new logo 
 import BusinessList from './components/BusinessList/BusinessList.js'; //imports BusinessList instance
 import SearchBar from './components/SearchBar/SearchBar.js'; //imports SearchBar instance
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
