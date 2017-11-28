353ea44f0f159a559d43e41a4340461be9fe21c3
Jae1xp <jae1xp@gmail.com>
2017-11-28 12:22:48 -0500

    modified apps.js and businesslist.js

--- src/App.js
+++ src/App.js
@@ -3,27 +3,20 @@ import './App.css'; //imports css styling
 //import logo from './logo.svg'; //imports the new logo 
 import BusinessList from './components/BusinessList/BusinessList.js'; //imports BusinessList instance
 import SearchBar from './components/SearchBar/SearchBar'; //imports SearchBar instance
-
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
-const businesses = [business, business, business, business, business, business];
+import Yelp from './util/Yelp';
 
 
 class App extends React.Component {
-	//let's go functionality
-	//searchYelp(term, location, sortBy) {
-		//console.log(`Searching Yelp for ${term}, in ${location}, ${sortBy}`);
-	//}
+	constructor(props) {
+    super(props);
+    this.state = {businesses: []};
+    this.searchYelp.bind(this);
+  }
+  searchYelp(term, location, sortBy) {
+		Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    })
+	}
   render() {
     return (
       <div className="App">
